7bce3aea25047952e010c8f226e4adc438eed48b
Anupam Ghosh <anupamghosh@N-Interns-224-AnupamGhosh.local>
2025-06-18 15:19:54 +0530

    added a generic converter which takes an object , and target object class and returns new target object type

--- src/main/java/com/increff/pos/util/ConvertUtil.java
+++ src/main/java/com/increff/pos/util/ConvertUtil.java
@@ -1,19 +1,39 @@
 package com.increff.pos.util;
 
-import com.increff.pos.model.data.ClientData;
-import com.increff.pos.model.form.ClientForm;
-import com.increff.pos.pojo.ClientPojo;
+import com.increff.pos.exception.ApiException;
+
+import java.lang.reflect.Field;
 
 public class ConvertUtil {
-    public static ClientPojo formToPojo(ClientForm f) {
-        ClientPojo p = new ClientPojo();
-        p.setName(f.getName().trim());
-        return p;
-    }
-    public static ClientData pojoToData(ClientPojo p) {
-        return ClientData.builder()
-                .id(p.getId())
-                .name(p.getName())
-                .build();
+
+    public static <T> T convert(Object source, Class<T> targetClass) throws ApiException {
+        if (source == null || targetClass == null) {
+            throw new ApiException("Source or target class must not be null");
+        }
+
+        try {
+            T target = targetClass.newInstance(); // for Java 1.8
+
+            Field[] sourceFields = source.getClass().getDeclaredFields();
+            Field[] targetFields = targetClass.getDeclaredFields();
+
+            for (Field sf : sourceFields) {
+                sf.setAccessible(true);
+                Object value = sf.get(source);
+
+                for (Field tf : targetFields) {
+                    if (sf.getName().equals(tf.getName()) &&
+                            sf.getType().equals(tf.getType())) {
+
+                        tf.setAccessible(true);
+                        tf.set(target, value);
+                        break;
+                    }
+                }
+            }
+            return target;
+        } catch (Exception e) {
+            throw new ApiException("Failed to convert: " + e.getMessage());
+        }
     }
 }
