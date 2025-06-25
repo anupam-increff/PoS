19690b3e5ad480972bd1cdc7bbc179c030809d13
Anupam Ghosh <anupamghosh@N-Interns-224-AnupamGhosh.local>
2025-06-25 16:06:56 +0530

    changed the response in service to dto invoice layer

--- pos-app/src/main/java/com/increff/pos/dto/InvoiceDto.java
+++ pos-app/src/main/java/com/increff/pos/dto/InvoiceDto.java
@@ -2,6 +2,8 @@ package com.increff.pos.dto;
 
 import com.increff.pos.service.InvoiceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,10 @@ public class InvoiceDto {
     @Autowired
     InvoiceService invoiceService;
     public ResponseEntity<byte[]> downloadInvoice(Integer orderId){
-        return  invoiceService.downloadInvoice(orderId);
+        byte[] invoice = invoiceService.downloadInvoice(orderId);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=order-" + orderId + ".pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(invoice);
     }
 }
--- pos-app/src/main/java/com/increff/pos/service/InvoiceService.java
+++ pos-app/src/main/java/com/increff/pos/service/InvoiceService.java
@@ -4,10 +4,6 @@ import com.increff.pos.exception.ApiException;
 import com.increff.pos.flow.OrderFlow;
 import com.increff.pos.pojo.OrderPojo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -20,7 +16,7 @@ public class InvoiceService {
     @Autowired
     private OrderFlow orderFlow;
 
-    public ResponseEntity<byte[]> downloadInvoice(Integer orderId) throws ApiException {
+    public byte[] downloadInvoice(Integer orderId) throws ApiException {
         OrderPojo order = orderFlow.get(orderId);
 
         if (order == null) {
@@ -37,10 +33,7 @@ public class InvoiceService {
 
         try {
             byte[] pdf = Files.readAllBytes(Paths.get(order.getInvoicePath()));
-            return  ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=order-" + orderId + ".pdf")
-                    .contentType(MediaType.APPLICATION_PDF)
-                    .body(pdf);
+            return pdf;
         } catch (IOException e) {
             throw new ApiException("Invoice could not be read or regenerated: " + e.getMessage());
         }
